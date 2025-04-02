6ebe2ff680c244f24abbcd2434e51a087120a307
kuza <kuzn03@vse.cz>
2025-04-02 22:09:45 +0200

    cviko

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ import { renderFile } from "ejs"
 import { drizzle } from "drizzle-orm/libsql"
 import { todosTable } from "./src/schema.js"
 import { eq } from "drizzle-orm"
+import { createNodeWebSocket } from '@hono/node-ws'
+import { WSContext } from "hono/ws"
 
 const db = drizzle({
   connection: "file:db.sqlite",
@@ -13,6 +15,10 @@ const db = drizzle({
 })
 
 const app = new Hono()
+
+
+const { injectWebSocket, upgradeWebSocket } = 
+  createNodeWebSocket({ app })
 
 app.use(logger())
 app.use(serveStatic({ root: "public" }))
@@ -85,6 +91,8 @@ app.get("/todos/:id/toggle", async (c) => {
     .set({ done: !todo.done })
     .where(eq(todosTable.id, id))
 
+  sendTodosToAllConnections()
+  
   return c.redirect(c.req.header("Referer"))
 })
 
@@ -100,11 +108,41 @@ app.get("/todos/:id/remove", async (c) => {
   return c.redirect("/")
 })
 
-serve(app, (info) => {
+
+
+//** @type{Set<WSContext<WebSocket>>} */
+const connections = new Set() 
+
+
+
+app.get("/ws", upgradeWebSocket((c) => {
+  console.log(c.req.path)
+  return {
+     onOpen: (ev, ws) => {
+      connections.add(ws)
+      console.log("onOpen")
+      
+     },
+     onClose: (evt, ws) => {
+      connections.delete(ws)
+      console.log("onClose")
+     },
+     onMessage: (evt, ws) => {
+      console.log("onMessage",evt.data)
+     }
+  }
+  })
+)
+
+
+const server = serve(app, (info) => {
   console.log(
     `App started on http://localhost:${info.port}`
   )
 })
+
+injectWebSocket(server)
+
 
 const getTodoById = async (id) => {
   const todo = await db
@@ -114,4 +152,20 @@ const getTodoById = async (id) => {
     .get()
 
   return todo
+}
+
+const sendTodosToAllConnections = async () => {
+  const todos =  await db.select().from(todosTable).all()
+
+  const rendered = await renderFile("views/_todos.html", {
+    todos
+  })
+
+  for (const connection of connections.values()){
+    const data = JSON.stringify({
+      type: "todos",
+      html: rendered,
+    })
+    connection.send(rendered)
+  }
 }
