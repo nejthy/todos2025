f0daa123a1f75f3adb74a7ce6f884a9b83884b0d
kuza <kuzn03@vse.cz>
2025-05-07 13:47:26 +0200

    testy

--- e2e/todos.spec.js
+++ e2e/todos.spec.js
@@ -11,8 +11,78 @@ test("form on index page creates new todos", async ({
 }) => {
   await page.goto("/")
 
-  await page.getByLabel("Název todo").fill("E2E todo")
+  await page.getByRole('textbox').fill("E2E todo")
   await page.getByText("Přidat todo").click()
 
   await expect(page.getByText("E2E todo")).toBeVisible()
 })
+
+
+test("check text edit ", async ({ page }) => {
+  await page.goto("/")
+
+  await page.getByText("E2E todo").click()
+
+  await expect(page.getByText("Upravit todočko")).toBeVisible()
+
+
+})
+
+test("Change title on todo", async ({ page }) => {
+  await page.goto("/")
+
+  await page.getByText("E2E todo").click()
+
+  await page.getByRole('textbox', { name: 'Titulek' }).fill("Koupit psa")
+
+  await page.getByText("Uložit").click()
+
+})
+
+test("Change priority", async ({ page }) => {
+  await page.goto("/")
+
+  await page.getByText("Koupit psa").click()
+
+  const select = page.getByLabel(/Priorita/i)
+  await expect(select).toBeVisible()
+  await select.selectOption({ label: "Low" })
+
+  await page.getByText("Uložit").click()
+
+  await expect(page.getByText("Priorita: low")).toBeVisible()
+
+})
+
+
+test("Change from undone to done", async ({ page }) => {
+  await page.goto("/")
+
+  await page.getByText("Koupit psa").click()
+
+  await page.getByText("Nedokončeno").click()
+
+  await expect(page.getByText("Dokončeno")).toBeVisible()
+
+
+})
+
+test("Delete todo", async ({ page }) => {
+  await page.goto("/")
+
+  await page.getByText("Koupit psa").click()
+
+  await page.getByText("odebrat").click()
+
+  await page.goto("/")
+
+  await expect(page.getByText("MY TODO APP")).toBeDefined()
+
+  await expect(page.getByText("Koupit psa")).toHaveCount(0)
+
+
+})
+
+
+
+
