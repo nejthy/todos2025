22a8560f0e7f4792f7c2bd172d25eee416b00cae
kuza <kuzn03@vse.cz>
2025-05-31 21:04:39 +0200

    testy

--- e2e/todos.spec.js
+++ e2e/todos.spec.js
@@ -1,59 +1,53 @@
 import { test, expect } from "@playwright/test"
 
+// Uložíme si storageState (pro session login) do souboru
+test('user can register and see "Oblíbené recepty"', async ({ page }) => {
+  await page.goto('/')
+  await page.getByText("Registrovat se").click()
+  await page.getByRole('textbox', { name: 'Jméno' }).fill("Adik")
+  await page.getByRole('textbox', { name: 'Heslo', exact: true }).fill("Adik")
+  await page.getByRole('textbox', { name: 'Potvrdit heslo', exact: true }).fill("Adik")
+  await page.getByText("Registrovat se").click()
 
+  // Ověříme, že je přihlášený uživatel a má vidět "Oblíbené recepty"
+  await expect(page.getByText("Oblíbené recepty")).toBeVisible()
 
-test("index page has title", async ({ page }) => {
-  await page.goto("/")
-
-  await expect(page.getByText("RECIPES")).toBeDefined()
 })
 
-test("form on index page creates new todos", async ({
-  page,
-}) => {
-  await page.getByRole('textbox').fill("E2E todo")
-  await page.getByText("Přidat todo").click()
-  await expect(page.getByText("E2E todo")).toBeVisible()
-})
 
+test('user login and can create a new recipe', async ({ page }) => {
+  await page.goto('/')
 
-test("check text edit ", async ({ page }) => {
-  await page.getByText("E2E todo").click()
-  await expect(page.getByText("Upravit todočko")).toBeVisible()
-})
+  await page.getByText("Přihlásit se").click()
+  await page.getByRole('textbox', { name: 'Jméno' }).fill("Adik")
+  await page.getByRole('textbox', { name: 'Heslo', exact: true }).fill("Adik")
+  await page.getByText("Přihlásit se").click()
 
-test("Change title on recipe", async ({ page }) => {
-  await page.getByText("E2E todo").click()
-  await page.getByRole('textbox', { name: 'Titulek' }).fill("Koupit psa")
+  await page.getByText("Přidat recept").click()
+  await page.getByRole('textbox', { name: 'Název receptu' }).fill("Můj testovací recept")
+  await page.getByRole('textbox', { name: 'Napiš ingredienci' }).fill("1. ingredience, 2. ingredience")
+  await page.getByLabel('Kategorie').selectOption('Snídaně')
+  await page.getByRole('textbox', { name: 'Postup' }).fill("1. krok, 2. krok")
   await page.getByText("Uložit").click()
 
+
 })
 
-test("Change ingredients", async ({ page }) => {
-  await page.getByText("Koupit psa").click()
-  const select = page.getByLabel(/Priorita/i)
-  await expect(select).toBeVisible()
-  await select.selectOption({ label: "Low" })
+test('user can edit recipe', async ({ page }) => {
+  await page.goto('/')
+  await page.getByText("Přihlásit se").click()
+  await page.getByRole('textbox', { name: 'Jméno' }).fill("Adik")
+  await page.getByRole('textbox', { name: 'Heslo', exact: true }).fill("Adik")
+  await page.getByText("Přihlásit se").click()
+
+  // Klikneme na recept
+  await page.getByText("Můj testovací recept").click()
+  await page.getByRole('button', { name: 'Upravit recept' }).click()
+  await page.getByRole('textbox', { name: 'Název receptu' }).fill("Můj upravený recept")
   await page.getByText("Uložit").click()
-  await expect(page.getByText("Priorita: low")).toBeVisible()
 
+  // Ověříme, že se změnil
+  await expect(page.getByText("Můj upravený recept")).toBeVisible()
 })
-
-
-test("Give like", async ({ page }) => {
-  await page.getByText("Koupit psa").click()
-  await page.getByText("Nedokončeno").click()
-  await expect(page.getByText("Dokončeno")).toBeVisible()
-})
-
-test("Delete recipe", async ({ page }) => {
-  await page.getByText("Koupit psa").click()
-  await page.getByText("odebrat").click()
-  await page.goto("/")
-  await expect(page.getByText("MY TODO APP")).toBeVisible()
-  await expect(page.getByText("Koupit psa")).not.toBeVisible()
-})
-
-
 
 
--- playwright.config.js
+++ playwright.config.js
@@ -17,7 +17,7 @@ export default defineConfig({
   /* Wait for maximum of 5s before marking test as failed */
   timeout: 5_000,
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
